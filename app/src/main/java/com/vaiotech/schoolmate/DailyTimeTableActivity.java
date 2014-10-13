f3a756e3336c4c411eca766b25681d5f7e26e0f0
kanaiyathacker <kanaiyalal.thacker@zonal.com>
2014-10-13 16:26:18 +0100

    Commit files

--- app/src/main/java/com/vaiotech/schoolmate/DailyTimeTableActivity.java
+++ app/src/main/java/com/vaiotech/schoolmate/DailyTimeTableActivity.java
@@ -1,17 +1,45 @@
 package com.vaiotech.schoolmate;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
+import com.vaiotech.bean.Student;
 import com.vaiotech.myschool.R;
+import com.vaiotech.services.DayTimeTableService;
+import com.vaiotech.services.RestService;
+import com.vaiotech.services.WeeklyTimeTableService;
+
+import java.util.List;
 
 public class DailyTimeTableActivity extends Activity {
+
+    private SpiceManager spiceManager = new SpiceManager(RestService.class);
+    private DayTimeTableService dayTimeTableService;
+    public static final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_time_table);
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
+        String studentInfoJson = sharedPreferences.getString("STUDENT_INFO" , null);
+        Student studentInfo = new Gson().fromJson(studentInfoJson , Student.class);
+        dayTimeTableService = new DayTimeTableService(studentInfo.getSchoolId() , studentInfo.getClassName() , studentInfo.getSection() , "Monday");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        spiceManager.start(this);
+        spiceManager.execute(dayTimeTableService , new RestServiceListener());
     }
 
 
@@ -32,5 +60,18 @@ public class DailyTimeTableActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public final class RestServiceListener implements RequestListener<List> {
+
+        @Override
+        public void onRequestFailure(SpiceException spiceException) {
+            Toast.makeText(DailyTimeTableActivity.this, "failure", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRequestSuccess(final List result) {
+            System.out.println("result... " + result);
+        }
     }
 }
