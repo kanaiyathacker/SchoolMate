290df4404dd6b992bb8abc82151123c588a8bdbb
kanaiyathacker <kanaiyalal.thacker@zonal.com>
2014-10-28 23:17:11 +0000

    commit

--- app/src/main/java/com/vaiotech/schoolmate/ResultDetailsActivity.java
+++ app/src/main/java/com/vaiotech/schoolmate/ResultDetailsActivity.java
@@ -2,33 +2,41 @@ package com.vaiotech.schoolmate;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.octo.android.robospice.SpiceManager;
 import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.vaiotech.bean.Item;
+import com.vaiotech.bean.ItemAdapter;
 import com.vaiotech.bean.Student;
 import com.vaiotech.myschool.R;
 import com.vaiotech.services.RestService;
 import com.vaiotech.services.ResultDetailsService;
 import com.vaiotech.services.ResultsService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ResultDetailsActivity extends Activity {
+public class ResultDetailsActivity extends Activity implements AdapterView.OnItemClickListener {
 
     private SpiceManager spiceManager = new SpiceManager(RestService.class);
     private ResultDetailsService resultDetailsService;
     private Context context;
     public static final String PREFS_NAME = "MyPrefsFile";
+    public ListView listView ;
+    List<Item> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +59,14 @@ public class ResultDetailsActivity extends Activity {
 
         resultDetailsService = new ResultDetailsService(studentInfo.getId() , studentInfo.getSchoolId() , studentInfo.getClassName() , studentInfo.getSection() , "T1");
         context = this;
+
+
         String type = getIntent().getStringExtra("TYPE");
         System.out.println("Type... "  + type);
-        TextView textViewTermHeader = (TextView)findViewById(R.id.textViewTermHeader);
-        textViewTermHeader.setText(type);
-
-        context = this;
+//        TextView  textViewTermHeader = (TextView)findViewById(R.id.textViewTermHeader);
+//        textViewTermHeader.setText(type);
+        listView = (ListView)findViewById(R.id.listView);
+        listView.setOnItemClickListener(this);
     }
 
     @Override
@@ -72,6 +82,16 @@ public class ResultDetailsActivity extends Activity {
         spiceManager.shouldStop();
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//        Toast toast = Toast.makeText(getApplicationContext(),
+//                "Item " + (position + 1) + ": " + listView.get(position),
+//                Toast.LENGTH_SHORT);
+//        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+//        toast.show();
+//    }
+    }
+
     private class RestServiceListener implements com.octo.android.robospice.request.listener.RequestListener<List> {
 
         @Override
@@ -83,84 +103,17 @@ public class ResultDetailsActivity extends Activity {
         public void onRequestSuccess(List result) {
             System.out.println(result);
             int count =1;
+            list = new ArrayList<Item>();
             for(Object currVal :result) {
                 Map map = (Map) currVal;
-                System.out.println("map..." + map);
-                String type = (String) map.get("type");
-                TextView textViewNameSub = null;
-                TextView textViewNameScore = null;
-                RelativeLayout rlo = null;
-                switch (count++) {
-                    case 1:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubOne);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreOne);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeOne);
-                        break;
-                    case 2:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubTwo);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreTwo);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeTwo);
-                        break;
-                    case 3:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubThree);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreThree);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeThree);
-                        break;
-                    case 4:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubFour);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreFour);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeFour);
-                        break;
-                    case 5:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubFive);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreFive);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeFive);
-                        break;
-                    case 6:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubSix);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreSix);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeSix);
-                        break;
-                    case 7:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubSeven);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreSeven);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeSeven);
-                        break;
-                    case 8:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubEight);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreEight);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeEight);
-                        break;
-                    case 9:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubNine);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreNine);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeNine);
-                        break;
-                    case 10:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubTen);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreTen);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeTen);
-                        break;
-                    case 11:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubEleven);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreEleven);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeEleven);
-                        break;
-                    case 12:
-                        textViewNameSub = (TextView)findViewById(R.id.textViewNameSubTwelve);
-                        textViewNameScore = (TextView)findViewById(R.id.textViewNameScoreTweleve);
-                        rlo = (RelativeLayout)findViewById(R.id.innerRelativeTwelve);
-                        break;
-                     default:
-                        break;
-                }
-
-                textViewNameSub.setText(""+map.get("subject"));
-                textViewNameScore.setText("Your score :" + map.get("scored") + "/" + map.get("total") );
-                rlo.setVisibility(View.VISIBLE);
+                Item item = new Item(""+map.get("subject") , "Your score :" + map.get("scored") + "/" + map.get("total"));
+                list.add(item);
             }
+            ItemAdapter adapter = new ItemAdapter(context , R.layout.list_item , list);
+            listView.setAdapter(adapter);
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -178,10 +131,5 @@ public class ResultDetailsActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void onClick(View view) {
-        Intent intent = new Intent(this , SubjectResultActivity.class);
-        startActivity(intent);
     }
 }
